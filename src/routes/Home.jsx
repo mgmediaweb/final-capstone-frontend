326fe38b23b472e30ba93ca714a4c9782fe92bfd
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-10-20 12:28:49 -0500

    Set up slider component in homepage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,51 @@
-const HomeScreen = () => {
-  const msj = 'home';
+import React, { useEffect } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Gallery from '../components/gallery/Gallery';
+
+function Home() {
+  const gallery = ['model3a.jpg', 'model3b.jpg', 'model3c.jpg', 'model3d.jpg'];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 760,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   return (
     <div className="container">
-      <h1>{msj}</h1>
+      <header>
+        <h1>LATEST MODELS</h1>
+        <p>Please select a car</p>
+        <span className="division-span">____________</span>
+      </header>
+      <ul className="slider">
+        <Slider {...settings}>
+          {
+            gallery.map((car, index) => (
+              <CarCard
+                key={car}
+                index={index}
+              />
+            ))
+          }
+        </Slider>
+      </ul>
     </div>
   );
-};
+}
 
-export default HomeScreen;
+export default Home;
