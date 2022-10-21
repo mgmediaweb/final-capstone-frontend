1add4307a9031f90269cef30e4b260e57c4c2e8e
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-10-21 10:38:40 -0500

    Fix scroll in homepage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-// import CarCard from '../components/carCard/CarCard';
 import carsData from '../mockData/carsData';
 import './Home.scss';
 import CarCard from '../components/carCard/CarCard';
