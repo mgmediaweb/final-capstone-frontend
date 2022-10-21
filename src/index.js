edb2bf3874719be17a933cc1d4a4a8b3e6790889
AntonioHincapie <marcohincapiem@gmail.com>
2022-10-21 12:34:39 -0500

    Fix fetch repetitions

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
