5fa8f132bf2fe9a9f4276a719338ba2f703e80ea
AntonioHincapie <marcohincapiem@gmail.com>
2022-10-24 16:19:07 -0500

    Fix linters

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -1,3 +1,4 @@
+/* eslint linebreak-style: ["error", "windows"] */
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint linebreak-style: ["error", "windows"] */
 import PropTypes from 'prop-types';
 
 const Form = (props) => {
